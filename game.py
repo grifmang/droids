3fccb69859dc26bfb20e7bf33252d3bbbfe9a057
Tim Griffith <grifmang@gmail.com>
2020-05-29 20:28:04 -0400

    enemy movement works

--- game.py
+++ game.py
@@ -17,6 +17,7 @@ def build_board(size, level, player_spot, enemy_spots):
 
     # Set enemy icon
     enemies = 'X'
+    enemy_locations = []
 
     # Set player spot and place player
     board[player_spot[0]][player_spot[1]] = player
@@ -29,8 +30,9 @@ def build_board(size, level, player_spot, enemy_spots):
         else:
             enemy_spots.insert(0, current_spot)
         board[spot[0]][spot[1]] = enemies
+        enemy_locations.append(board[spot[0]][spot[1]])
 
-    return [board, player_spot, enemy_spots]
+    return [board, player_spot, enemy_spots, enemy_locations]
 
 def move_player(player_spot, key):
     x_movements = {'up': -1, 'down': 1}
@@ -46,6 +48,19 @@ def enemy_spot(board_size):
     x = random.randint(0, board_size - 1)
     y = random.randint(0, board_size - 1)
     return [x,y]
+
+def move_enemy(player_spot, enemy_spot, board_length):
+    # for num in enemy_spot:
+    #     print(num)
+    if player_spot[0] < enemy_spot[0] and enemy_spot[0] != 0:
+        enemy_spot[0] -= 1
+    if player_spot[0] > enemy_spot[0] and enemy_spot[0] > board_length:
+        enemy_spot[0] += 1
+    if player_spot[1] < enemy_spot[1] and enemy_spot[1] != 0:
+        enemy_spot[1] -= 1
+    if player_spot[1] > enemy_spot[1] and enemy_spot[1] > board_length:
+        enemy_spot[1] += 1
+    return enemy_spot
 
 def game(board_size):
     player = chr(169)
@@ -65,6 +80,7 @@ def game(board_size):
     board = build_board(board_size, level, [board_size // 2, board_size // 2],  enemy_array)
     player_spot = board[1]
     enemy_spots = board[2]
+    enemy_locations = board[3]
 
     playing = True
     movements = {'up':'up', 'down':'down', 'left':'left', 'right':'right'}
@@ -81,8 +97,13 @@ def game(board_size):
         if keyboard.read_key() in movements:
             new_coords = move_player(board[1], keyboard.read_key())
             board = build_board(board_size, level, [new_coords[0], new_coords[1]], enemy_spots)
+            # print(enemy_spots)
+            for s in enemy_spots:
+                # print(s)
+                s = move_enemy(board[1], s, len(board[0]))
+            # board = build_board(board_size, level, [new_coords[0], new_coords[1]], enemy_spots)
             
-
+        
         if new_coords in enemy_spots:
             print('Game Over.')
             playing = False
